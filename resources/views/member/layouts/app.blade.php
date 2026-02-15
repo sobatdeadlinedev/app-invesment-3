9d0c786f2d911fb2f6442e82d20e594fd96b59bf
daniizzulhaq <daniizzulhaq2002tkj@gmail.com>
2026-02-15 18:58:39 +0700

    fix : title

--- resources/views/member/layouts/app.blade.php
+++ resources/views/member/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{ $appConfig['app_logo']['value'] }}" />
-    <title>STARS INVESTMENT</title>
+    <title>LVUUX EXCHANGE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
